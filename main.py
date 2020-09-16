16f1013d56f70a87e7c64fe98e48b909850b52cd
eim5178 <eim5178@psu.edu>
2020-09-16 14:12:26 +0000

    function name

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 # Author: Evelyn Moore eim5178@psu.edu
 # Collaborator:
 
-def sum_n(n):
+def digit_sum(n):
   if (n<=1):
     return n
   else:
     n = n%10
-    return n + sum_n(n-1)
+    return n + digit_sum(n-1)
 def run():
   n = int(input("Enter an int: "))
-  nsum = sum_n(n)
+  nsum = digit_sum(n)
   print(f"sum of the digits of {n} is {nsum}")
-  
+
 if __name__ == '__main__':
   run()
